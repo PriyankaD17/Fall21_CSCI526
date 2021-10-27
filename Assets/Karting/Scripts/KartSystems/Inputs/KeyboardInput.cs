7410b3c1d46335d237f25a9cb39550362e21392c
Priyanka Deshpande <psdeshpa@usc.edu>
2021-10-26 21:37:59 -0700

    added gitignore

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,60 @@
+﻿// using UnityEngine;
+// using EasyJoystick;
+
+// namespace KartGame.KartSystems {
+
+//     public class KeyboardInput : BaseInput
+//     {
+//         public Joystick joystick;
+
+//         public string TurnInputName = "Horizontal";
+//         public string AccelerateButtonName = "Accelerate";
+//         public string BrakeButtonName = "Brake";
+
+//         public override InputData GenerateInput() { 
+//             return new InputData
+//             {
+//                 Accelerate = joystick.Vertical() > 0 ? true : false,
+//                 Brake = joystick.Vertical() < 0 ? true : false,
+//                 TurnInput = joystick.Horizontal()
+//                 // Accelerate = Input.GetButton(AccelerateButtonName),
+//                 // Brake = Input.GetButton(BrakeButtonName),
+//                 // TurnInput = Input.GetAxis(TurnInputName)
+//             };
+//         }
+//     }
+// }   
+
+using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+        private float TurnFactor = 2f;
+
+        public override InputData GenerateInput() {
+            if (Application.platform != RuntimePlatform.Android)
+            {
+                return new InputData
+                {
+                    Accelerate = Input.GetButton(AccelerateButtonName),
+                    Brake = Input.GetButton(BrakeButtonName),
+                    TurnInput = Input.GetAxis("Horizontal")
+                };
+            } else
+            {
+                bool _acc = (Input.GetTouch(0).position.x > Screen.width / 2);
+                return new InputData
+                {
+                    Accelerate = _acc,
+                    Brake = !_acc,
+                    TurnInput = Mathf.Clamp(TurnFactor * Input.acceleration.normalized.x, -1f, 1f),
+                };
+            }
+        }
+    }
+}
---
+++ Assets/Karting/Scripts/KartSystems/Vibration.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+
+    public class Vibration : MonoBehaviour
+    {
+        private static bool vibrationSupported = false;
+        private static AndroidJavaObject vibrator;
+        private static AndroidJavaClass vibrationEffectClass;
+        private static int DEFAULT_AMPLITUDE;
+
+        // Initialization before scene loads
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialise()
+        {
+            if (Application.platform == RuntimePlatform.Android &&
+                    (new AndroidJavaClass("android.os.Build$VERSION")).GetStatic<int>("SDK_INT") >= 26)
+                vibrationSupported = true;
+            else return;
+
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+            vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
+            DEFAULT_AMPLITUDE = vibrationEffectClass.GetStatic<int>("DEFAULT_AMPLITUDE");
+            Handheld.Vibrate();
+        }
+
+        private static void SystemVibrate(string android_function, params object[] args)
+        {
+            if (!vibrationSupported)
+                return;
+
+            AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>(android_function, args);
+            vibrator.Call("vibrate", vibrationEffect);
+        }
+
+        public static void Vibrate(long duration, int amplitude=0)
+        {
+            if (!vibrationSupported)
+                return;
+
+            if (amplitude == 0)
+                amplitude = DEFAULT_AMPLITUDE;
+            SystemVibrate("createOneShot", new object[] { duration, amplitude });
+        }
+
+        public static void Cancel()
+        {
+            if (!vibrationSupported)
+                return;
+
+            vibrator.Call("cancel");
+        }
+    }
+}
